2958fd3f6d9ab9ed5735b5437c3ce7365c82f81d
Daniela <danielaparra1104@gmail.com>
2026-05-24 00:19:28 -0500

    Creacion de entorno y actualizacion

--- app.py
+++ app.py
@@ -354,5 +354,6 @@ def consulta_dinamica_olap():
         return f"Error interno en el servidor analítico: {e}", 500
 
 if __name__ == '__main__':
+    
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
